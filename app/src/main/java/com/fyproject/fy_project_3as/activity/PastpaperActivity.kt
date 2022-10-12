a42bb072aefe49f8054d1b112f0dd1d5325a49eb
thushanisuresh <thushanisuresh66@gmail.com>
2022-10-12 20:04:26 +0530

    created pastpaper screen

---
+++ app/src/main/java/com/fyproject/fy_project_3as/activity/PastpaperActivity.kt
@@ -0,0 +1,12 @@
+package com.fyproject.fy_project_3as.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fyproject.fy_project_3as.R
+
+class PastpaperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pastpaper)
+    }
+}
