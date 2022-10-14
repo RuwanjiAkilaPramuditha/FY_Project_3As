7ce936098ec6912b1a7bbd1d053b422768de7b65
tharinda97 <tharindakodikara@outlook.com>
2022-10-14 18:50:23 +0530

    created subject one topic

---
+++ app/src/main/java/com/fyproject/fy_project_3as/model/SubjectOneTopics.kt
@@ -0,0 +1,3 @@
+package com.fyproject.fy_project_3as.model
+
+data class SubjectOneTopics(var subTopic: String ?= null, var topic: String ?= null, var hours: Long ?= null, var completed: Boolean ?= null,)
