e5978709996a0a6a25fea6eb4c8ea58e80ae6cec
tharinda97 <tharindakodikara@outlook.com>
2022-10-15 05:02:32 +0530

    resolved project errors

--- app/src/main/java/com/fyproject/fy_project_3as/activity/ProfileActivity.kt
+++ app/src/main/java/com/fyproject/fy_project_3as/activity/ProfileActivity.kt
@@ -2,6 +2,7 @@ package com.fyproject.fy_project_3as.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fyproject.fy_project_3as.R
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
